f8198191007295642151e2c9bc59c865c69059f3
sufirang <sufiabano1996@gmail.com>
2023-08-26 22:52:14 +0530

    bookstore

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchBar from './Components/SearchBar';
+import "./style.css"
+function App() {
+  return (
+    <div className="App">
+        <SearchBar />
+    </div>
+  );
+}
+
+export default App;
